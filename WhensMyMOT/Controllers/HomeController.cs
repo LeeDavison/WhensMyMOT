52594c90f735bb185fbc0176b2503173e3c80df2
Lee Davison <lee@davmail.co.uk>
2020-12-17 15:01:58 +0000

    Add project files.

---
+++ WhensMyMOT/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WhensMyMOT.Models;
+
+namespace WhensMyMOT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CheckRegistration(Registration registration)
+        {
+            // Reset error state
+            registration.ErrorMessage = null;
+
+            // Reset if invalid input
+            if (!ModelState.IsValid)
+            {
+                registration.Make = null;
+                registration.Model = null;
+                registration.Colour = null;
+                registration.ExpiryDate = null;
+                registration.Mileage = null;
+                registration.ErrorMessage = null;
+            }
+            else
+            {
+                // Input good, submit request
+                string ApiURL = "https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration=" + registration.RegNumber;
+
+                try
+                {
+                    using (var httpClient = new HttpClient())
+                    {
+                        httpClient.DefaultRequestHeaders.Add("Accept", "application/json+v6");
+                        httpClient.DefaultRequestHeaders.Add("x-api-key", "fZi8YcjrZN1cGkQeZP7Uaa4rTxua8HovaswPuIno");
+
+                        var response = await httpClient.GetAsync(ApiURL);
+                        if (response.IsSuccessStatusCode)
+                        {
+                            string apiResponse = await response.Content.ReadAsStringAsync();
+                            apiResponse = apiResponse.Substring(1, apiResponse.Length - 2);
+                            JObject JsonObject = JObject.Parse(apiResponse);
+                            registration.Make = (string)JsonObject.SelectToken("make");
+                            registration.Model = (string)JsonObject.SelectToken("model");
+                            registration.Colour = (string)JsonObject.SelectToken("primaryColour");
+                            JArray MotTests = (JArray)JsonObject.SelectToken("motTests ");
+                            registration.ExpiryDate = (string)MotTests[0].SelectToken("expiryDate");
+                            registration.Mileage = (string)MotTests[0].SelectToken("odometerValue");
+                        }
+                        else
+                        {
+                            registration.ErrorMessage = "Sorry, we were unable to complete your request, please try again. If the problem persists please contact us.";
+                        }
+                    }
+                }
+                catch
+                {
+                    registration.Make = null;
+                    registration.Model = null;
+                    registration.Colour = null;
+                    registration.ExpiryDate = null;
+                    registration.Mileage = null;
+                    registration.ErrorMessage = "Sorry, a system error has occured - please contact our support department for further guidance.";
+                }                
+            }
+
+            return View("Index", registration);
+        }
+    }
+}
---
+++ WhensMyMOT/Models/Registration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhensMyMOT.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = ("Registration number is required"))]
+        [RegularExpression("(?<Current>^[A-Za-z]{2}[0-9]{2}[A-Za-z]{3}$)|(?<Prefix>^[A-Za-z][0-9]{1,3}[A-Za-z]{3}$)|(?<Suffix>^[A-Za-z]{3}[0-9]{1,3}[A-Za-z]$)|(?<DatelessLongNumberPrefix>^[0-9]{1,4}[A-Za-z]{1,2}$)|(?<DatelessShortNumberPrefix>^[0-9]{1,3}[A-Za-z]{1,3}$)|(?<DatelessLongNumberSuffix>^[A-Za-z]{1,2}[0-9]{1,4}$)|(?<DatelessShortNumberSufix>^[A-Za-z]{1,3}[0-9]{1,3}$)|(?<DatelessNorthernIreland>^[A-Za-z]{1,3}[0-9]{1,4}$)|(?<DiplomaticPlate>^[0-9]{3}[DX]{1}[0-9]{3}$)", ErrorMessage = "Invalid UK registration number")]
+        public string RegNumber { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Colour { get; set; }
+
+        public string ExpiryDate { get; set; }
+
+        public string Mileage { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
